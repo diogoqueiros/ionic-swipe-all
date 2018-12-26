e5294354134e0ee6cfd9471a2685d1a9cd4b4988
Diogo Queirós <diogo.queiros@tlantic.com>
2018-12-26 11:54:53 +0000

    refactor: remove common module import

--- src/module.ts
+++ src/module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { IonicSwipeAll } from './ionic.swipe.all';
 
 @NgModule({
   declarations: [IonicSwipeAll],
   exports: [IonicSwipeAll],
-  imports: [CommonModule],
 })
 
 export class IonicSwipeAllModule {
